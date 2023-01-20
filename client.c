4c3e9dc370e1fcc0bba6ecfc3bd2071ffc7e0068
Liam PRADENE <lpradene@e1r7p10.clusters.42paris.fr>
2023-01-20 10:02:47 +0100

    beginning of the norminette

--- client.c
+++ client.c
@@ -1,110 +1,110 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   main.c                                             :+:      :+:    :+:   */
+/*   client.c                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: lpradene <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/01/16 22:39:15 by lpradene          #+#    #+#             */
-/*   Updated: 2023/01/16 22:39:16 by lpradene         ###   ########.fr       */
+/*   Created: 2023/01/19 19:31:55 by lpradene          #+#    #+#             */
+/*   Updated: 2023/01/19 19:44:34 by lpradene         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "client.h"
 
-int received = 0;
+int	g_received = 0;
 
-void    error(char *s)
+void	error(char *s)
 {
-    printf("%s\n", s);
-    exit(EXIT_FAILURE);
+	printf("%s\n", s);
+	exit(EXIT_FAILURE);
 }
 
-void    handle_signal(int signal, siginfo_t *client, void *ucontext)
+void	handle_signal(int signal, siginfo_t *client, void *ucontext)
 {
-    (void)ucontext;
-    if (signal == SIGUSR1)
-        received = 1;
-    else
-        printf("The server received the string\n");
+	(void)ucontext;
+	if (signal == SIGUSR1)
+		g_received = 1;
+	else
+		printf("The server received the string\n");
 }
 
-void    send_size(int pid, int size)
+void	send_size(int pid, int size)
 {
-    int i;
+	int	i;
 
-    i = -1;
-    while (++i < 32)
-    {
-        received = 0;
-        if (size & 0x01)
-        {
-            if (kill(pid, SIGUSR1) == -1)
-                error("Error");
-        }
-        else
-        {
-            if (kill(pid, SIGUSR2) == -1)
-                error("Error");
-        }
-        if (!received)
-            pause();
-        size = size >> 1;
-    }
+	i = -1;
+	while (++i < 32)
+	{
+		g_received = 0;
+		if (size & 0x01)
+		{
+			if (kill(pid, SIGUSR1) == -1)
+				error("Error");
+		}
+		else
+		{
+			if (kill(pid, SIGUSR2) == -1)
+				error("Error");
+		}
+		if (!g_received)
+			pause();
+		size = size >> 1;
+	}
 }
 
-void    send_char(int pid, unsigned char c)
+void	send_char(int pid, unsigned char c)
 {
-    int bit;
+	int	i;
 
-    bit = 1 << 8;
-    while (bit)
-    {
-        received = 0;
-        if (bit & c)
-        {
-            if (kill(pid, SIGUSR1) == -1)
-                error("Error");
-        }
-        else
-        {
-            if (kill(pid, SIGUSR2) == -1)
-                error("Error");
-        }
-        if (!received)
-            pause();
-        bit = bit >> 1;
-    }
+	i = 1 << 8;
+	while (i)
+	{
+		g_received = 0;
+		if (c & i)
+		{
+			if (kill(pid, SIGUSR1) == -1)
+				error("Error");
+		}
+		else
+		{
+			if (kill(pid, SIGUSR2) == -1)
+				error("Error");
+		}
+		if (!g_received)
+			pause();
+		i = i >> 1;
+	}
 }
 
-void    send_string(int pid, unsigned char *s)
+void	send_string(int pid, unsigned char *s)
 {
-    int i;
+	int	i;
 
-    send_size(pid, strlen(s));
-    i = -1;
-    while(s[++i])
-        send_char(pid, s[i]);
-    send_char(pid, 0);
+	send_size(pid, strlen(s));
+	i = -1;
+	while (s[++i])
+		send_char(pid, s[i]);
+	send_char(pid, 0);
 }
 
-int main(int argc, char **argv)
+int	main(int argc, char **argv)
 {
-    int	                pid;
-    struct sigaction    sig;
+	int					pid;
+	struct sigaction	sig;
 
-    if (argc != 3)
-        error("You have to run the program with : ./client [PID] [STRING]");
-    pid = atoi(argv[1]);
-    if (pid <= 0)
-        error("Invalid PID");
-    sig.sa_flags = SA_SIGINFO;
-    sig.sa_sigaction = handle_signal;
-    sigemptyset(&sig.sa_mask);
-    sigaddset(&sig.sa_mask, SIGUSR1);
-    sigaddset(&sig.sa_mask, SIGUSR2);
-    sigaction(SIGUSR1, &sig, NULL);
-    sigaction(SIGUSR2, &sig, NULL);
-    send_string(pid, argv[2]);
-    return (0);
+	if (argc != 3)
+		error("You have to run the program with : ./client [PID] [STRING]");
+	pid = atoi(argv[1]);
+	if (pid <= 0)
+		error("Invalid PID");
+	sig.sa_flags = SA_SIGINFO;
+	sig.sa_sigaction = handle_signal;
+	sigemptyset(&sig.sa_mask);
+	sigaddset(&sig.sa_mask, SIGUSR1);
+	sigaddset(&sig.sa_mask, SIGUSR2);
+	sigaction(SIGUSR1, &sig, NULL);
+	sigaction(SIGUSR2, &sig, NULL);
+	send_string(pid, argv[2]);
+	return (0);
 }
--- server.c
+++ server.c
@@ -1,102 +1,102 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   main.c                                             :+:      :+:    :+:   */
+/*   server.c                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: lpradene <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/01/16 22:28:28 by lpradene          #+#    #+#             */
-/*   Updated: 2023/01/16 22:28:30 by lpradene         ###   ########.fr       */
+/*   Created: 2023/01/19 19:30:07 by lpradene          #+#    #+#             */
+/*   Updated: 2023/01/19 19:31:10 by lpradene         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "server.h"
 
-int     pow_2(int n)
+int	pow_2(int n)
 {
-    int nb;
+	int	nb;
 
-    if (n == 0)
-        return (1);
-    nb = 2;
-    while (--n)
-        nb *= 2;
-    return (nb);
+	if (n == 0)
+		return (1);
+	nb = 2;
+	while (--n)
+		nb *= 2;
+	return (nb);
 }
 
-void    getlen(int sig, unsigned char **str, int *received)
+void	getlen(int sig, unsigned char **str, int *received)
 {
-    static int  size = 0;
-    static int  i = 0;
+	static int	size = 0;
+	static int	i = 0;
 
-    if (sig == SIGUSR1)
-        size += pow_2(i);
-    if (i == 31)
-    {
-        *received = 1;
-        *str = malloc(size + 1);
-        (*str)[size] = 0;
-        size = 0;
-        i = 0;
-        return ;
-    }
-    i++;
+	if (sig == SIGUSR1)
+		size += pow_2(i);
+	if (i == 31)
+	{
+		*received = 1;
+		*str = malloc(size + 1);
+		(*str)[size] = 0;
+		size = 0;
+		i = 0;
+		return ;
+	}
+	i++;
 }
 
-void    print(int pid, unsigned char *s)
+void	print(int pid, unsigned char *s)
 {
-    printf("%s\n", s);
-    free(s);
-    kill(pid, SIGUSR2);
+	printf("%s\n", s);
+	free(s);
+	kill(pid, SIGUSR2);
 }
 
-void    handle_signal(int sig, siginfo_t *client, void *ucontext)
+void	handle_signal(int sig, siginfo_t *client, void *ucontext)
 {
-    (void)ucontext;
-    static unsigned char    *str;
-    static unsigned char    c = 0;
-    static int              i = 8;
-    static int              curr = 0;
-    static int              received = 0;
+	static unsigned char	*str;
+	static unsigned char	c = 0;
+	static int				i = 8;
+	static int				curr = 0;
+	static int				received = 0;
 
-    if (!received)
-        getlen(sig, &str, &received);
-    else
-    {
-        if (i < 0)
-            c = 0;
-        if (i < 0)
-            i = 8;
-        if (sig == SIGUSR1)
-            c += pow_2(i);
-        i--;
-        if (c && i < 0)
-            str[curr++] = c;
-        else if (!c && i < 0)
-        {
-            i = 8;
-            curr = 0;
-            received = 0;
-            print(client->si_pid, str);
-        }
-    }
-    kill(client->si_pid, SIGUSR1);
+	(void)ucontext;
+	if (!received)
+		getlen(sig, &str, &received);
+	else
+	{
+		if (i < 0)
+			c = 0;
+		if (i < 0)
+			i = 8;
+		if (sig == SIGUSR1)
+			c += pow_2(i);
+		i--;
+		if (c && i < 0)
+			str[curr++] = c;
+		else if (!c && i < 0)
+		{
+			i = 8;
+			curr = 0;
+			received = 0;
+			print(client->si_pid, str);
+		}
+	}
+	kill(client->si_pid, SIGUSR1);
 }
 
-int main(void)
+int	main(void)
 {
-    int                 pid;
-    struct sigaction    sig;
+	int					pid;
+	struct sigaction	sig;
 
-    sig.sa_flags = SA_SIGINFO;
-    sig.sa_sigaction = handle_signal;
-    sigemptyset(&sig.sa_mask);
-    sigaddset(&sig.sa_mask, SIGUSR1);
-    sigaddset(&sig.sa_mask, SIGUSR2);
-    sigaction(SIGUSR1, &sig, NULL);
-    sigaction(SIGUSR2, &sig, NULL);
-    pid = getpid();
-    printf("%d\n", pid);
-    while (1)
-        pause();
+	sig.sa_flags = SA_SIGINFO;
+	sig.sa_sigaction = handle_signal;
+	sigemptyset(&sig.sa_mask);
+	sigaddset(&sig.sa_mask, SIGUSR1);
+	sigaddset(&sig.sa_mask, SIGUSR2);
+	sigaction(SIGUSR1, &sig, NULL);
+	sigaction(SIGUSR2, &sig, NULL);
+	pid = getpid();
+	printf("%d\n", pid);
+	while (1)
+		pause();
 }
